2ba0ec5ed7bd9ab8e5db5ea417719bc7a0576764
Artem016 <artemklemonow@gmail.com>
2023-05-17 18:34:33 +0500

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RepairRequests2
+{       
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }//Стандартаная команда появления главного окна
+
+        private void AddRequest_Click(object sender, RoutedEventArgs e)
+        {
+            //С помощья Cntr + Shift + A мы создаем новое окно для программы.
+            AddRequestWindow addRequestWindow = new AddRequestWindow();
+            //Создаем обьект данного окна, при нажатии на кнопку, чтобы можно было показать его пользователю.
+            addRequestWindow.Show();//показываем пользователю окно
+            addRequestWindow.Owner = this;//иницаализация окна MainWindow как владельца окна AddRequestWindow, то есть при закрытии галвног окна, закрывается и дочернее, но не наоборот.
+        }
+
+        internal bool isDoneShow;
+
+        private void IsDoneShow_Checked(object sender, RoutedEventArgs e)
+        {
+            isDoneShow = true;
+        }
+    }
+}
---
+++ Request.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepairRequests2
+{
+    internal class Request
+    {
+        internal string fio;
+        internal string typeEquipmen;
+        internal string brandEquimen;
+        internal string modelEquimen;
+        internal string discriptionProblem;
+        internal bool isDone;
+        DateTime dateRequest;
+
+        internal Request(string fio, string typeEquipmen, string brandEquimen, string modelEquimen, string discriptionProblem, bool isDone, DateTime dateRequest)
+        {
+            this.fio = fio;
+            this.typeEquipmen = typeEquipmen;
+            this.brandEquimen = brandEquimen;
+            this.modelEquimen = modelEquimen;
+            this.discriptionProblem = discriptionProblem;
+            this.isDone = isDone;
+            this.dateRequest = dateRequest;
+        }
+    }
+}
